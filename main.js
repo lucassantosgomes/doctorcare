271684833e048d528a2ef0b2fd84da5011b270d1
lucassantosgomes <104720922+lucassantosgomes@users.noreply.github.com>
2022-05-05 18:32:17 -0300

    aula 4 - nlw return

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
+window.addEventListener('scroll', onScroll)
+onScroll();
 function onScroll() {
+  showNavOnScroll();
+  backToTopButtonOnScroll();
+}
+function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add('scroll');
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove('scroll');
+  }
+}
+function backToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
   }
 }
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('menu-expanded');
 }
 
 ScrollReveal({

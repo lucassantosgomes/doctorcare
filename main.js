95c1d5f6c0289a903387b1ff48fe2e78c294e801
lucassantosgomes <104720922+lucassantosgomes@users.noreply.github.com>
2022-05-13 20:23:27 -0300

    projeto doctorcare finalizado! (nlw return)

--- main.js
+++ main.js
@@ -1,28 +1,65 @@
 window.addEventListener('scroll', onScroll)
-onScroll();
+onScroll()
 function onScroll() {
-  showNavOnScroll();
-  backToTopButtonOnScroll();
+  showNavOnScroll()
+  backToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a seçao passou da linha
+
+  //topo da seçao
+  const sectionTop = section.offsetTop
+  //altura
+  const sectionHeight = section.offsetHeight
+  //o topo da seçao chegou ou ultrapassou a linha alvo;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //verifica se a base esta a baixo da seçao
+  const sectionEndAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndAt <= targetLine
+
+  // console.log('o topo da seçao chegou ou ultrapassou a linha? ',sectionEndPassedTargetLine);
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectioId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectioId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll');
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll');
+    navigation.classList.remove('scroll')
   }
 }
 function backToTopButtonOnScroll() {
   if (scrollY > 550) {
-    backToTopButton.classList.add('show');
+    backToTopButton.classList.add('show')
   } else {
-    backToTopButton.classList.remove('show');
+    backToTopButton.classList.remove('show')
   }
 }
 function openMenu() {
-  document.body.classList.add('menu-expanded');
+  document.body.classList.add('menu-expanded')
 }
 function closeMenu() {
-  document.body.classList.remove('menu-expanded');
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
